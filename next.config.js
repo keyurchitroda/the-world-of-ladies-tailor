0918b925ed4a23e2ab115586b2cd6fe6b8b91d97
Keyur Chitroda <keyur.chitroda@bluelabellabs.com>
2023-12-11 18:07:53 +0530

    create signup api and signin-singup UI

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  //   webpack: (config) => {
-  //     config.module.rules.push({
-  //       test: /\.(scss|css)$/,
-  //       use: ["style-loader", "css-loader"],
-  //     });
-  //     return config;
-  //   },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
---
+++ src/app/api/users/signin/route.ts
@@ -0,0 +1,47 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptJS from "bcryptjs";
+import jwt from "jsonwebtoken";
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "User does not exists" },
+        { status: 400 }
+      );
+    }
+
+    const validatePassword = await bcryptJS.compare(password, user.password);
+    if (!validatePassword) {
+      return NextResponse.json({ error: "Invalid password" }, { status: 400 });
+    }
+
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    const token = jwt.sign(tokenData, process.env.TOEKN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json(
+      { message: "Login successfull", success: true, data: user },
+      { status: 200 }
+    );
+
+    response.cookies.set("token", token, { httpOnly: true });
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
